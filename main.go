cc1b6934f271e569ad30362075dcaf173ee33aeb
Da-max <da-max@tutanota.com>
2021-11-06 15:28:04 +0000

    ➕ install and configure orm

--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
+	"fmt"
 	_ "mibde/routers"
+	"os"
+
+	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	beego.Run()
+func getDsn() string {
+	return os.Getenv("MARIADB_USER") + ":" + os.Getenv("MARIADB_PASSWORD") + "@/" + os.Getenv("MARIADB_DATABASE") + "?charset=utf8"
 }
 
+func init() {
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	fmt.Println(getDsn())
+	// 	orm.SetMaxIdleConns("default", 30)
+
+	orm.RegisterDataBase("default", "mysql", getDsn())
+
+}
+
+func main() {
+	// o := orm.NewOrm()
+
+	beego.Run()
+}
